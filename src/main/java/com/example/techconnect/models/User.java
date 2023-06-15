ab878091bdf5253aa6151965d2096f6afee2224e
Cole Usher <coleausher97@gmail.com>
2023-06-15 11:46:48 -0500

    added UserController, User model, UserRepository, Securityconfig

---
+++ src/main/java/com/example/techconnect/models/User.java
@@ -0,0 +1,58 @@
+package com.example.techconnect.models;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String profilePicture;
+
+    // This is known as a copy constructor, which will make a clone of the user object.
+    public User(User copy) {
+        this.id = copy.id;
+        this.email = copy.email;
+        this.username = copy.username;
+        this.password = copy.password;
+        this.firstName = copy.firstName;
+        this.lastName = copy.lastName;
+    }
+
+
+//    @OneToMany(cascade = CascadeType.ALL), mappedBy = "user")
+//    List<Event> events;
+
+
+}
---
+++ src/main/java/com/example/techconnect/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.techconnect.repositories;
+
+import com.example.techconnect.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
+}
