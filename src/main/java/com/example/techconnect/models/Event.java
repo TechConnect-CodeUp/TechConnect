df0312a7dfbeab0dc60cacc9f65a1c2e7fa31a77
chininchu <120990876+chininchu@users.noreply.github.com>
2023-06-15 12:01:45 -0500

    Currently working on creating an Event Model

--- src/main/java/com/example/techconnect/models/Event.java
+++ src/main/java/com/example/techconnect/models/Event.java
@@ -1,7 +1,45 @@
 package com.example.techconnect.models;
 
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+
+
+
+
 @Entity
+@Table(name = "events")
+
 public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long EventId;
+
+    private String title;
+    private String description;
+
+
+    // We will uncomment this relationship after a pull request
+
+//    @ManyToOne
+//    private User HostID;
+
+
+
+
+
+
+
+
 
 }
