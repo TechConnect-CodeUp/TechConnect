f6da43a0ee2e155a897b3e89c5c849b89dbb5593
chininchu <120990876+chininchu@users.noreply.github.com>
2023-06-15 15:10:12 -0500

    Created an Eventbean and an Event Repo

---
+++ src/main/java/com/example/techconnect/repositories/AttendeesRepository.java
@@ -0,0 +1,13 @@
+package com.example.techconnect.repositories;
+
+import com.example.techconnect.models.Attendees;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AttendeesRepository extends JpaRepository<Attendees,Long> {
+
+
+
+
+
+
+}
