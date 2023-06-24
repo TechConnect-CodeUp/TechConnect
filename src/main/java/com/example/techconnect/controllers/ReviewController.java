706f8c0aa25ce300ece895ffd68051524dc63970
chininchu <120990876+chininchu@users.noreply.github.com>
2023-06-23 19:02:55 -0500

    Commented out the ReviewController class and the event-reviews html file

--- src/main/java/com/example/techconnect/controllers/ReviewController.java
+++ src/main/java/com/example/techconnect/controllers/ReviewController.java
@@ -1,46 +1,48 @@
-package com.example.techconnect.controllers;
-
-import com.example.techconnect.models.Attendee;
-import com.example.techconnect.models.Event;
-import com.example.techconnect.repositories.EventRepository;
-import com.example.techconnect.repositories.ReviewRepository;
-import com.example.techconnect.repositories.UserRepository;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-import java.util.Collections;
-
-@Controller
-public class ReviewController {
-
-    private final EventRepository eventRepository;
-
-    private final ReviewRepository reviewRepository;
-
-    private final UserRepository userRepository;
-
-
-    public ReviewController(EventRepository eventRepository, UserRepository userRepository, ReviewRepository reviewRepository) {
-        this.eventRepository = eventRepository;
-        this.userRepository = userRepository;
-        this.reviewRepository = reviewRepository;
-
-    }
-
-    @GetMapping("/reviews/view/{id}")
-
-    public String showEventReview(@PathVariable long id, Model model) {
-
-        model.addAttribute("event",eventRepository.findById(id));
-
-        model.addAttribute("reviews", reviewRepository.findAllByEventEventId(id));
-
-        return "event-reviews";
-
-
-    }
-
-
-}
+//package com.example.techconnect.controllers;
+//
+//import com.example.techconnect.models.Attendee;
+//import com.example.techconnect.models.Event;
+//import com.example.techconnect.repositories.EventRepository;
+//import com.example.techconnect.repositories.ReviewRepository;
+//import com.example.techconnect.repositories.UserRepository;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//
+//import java.util.Collections;
+//
+//@Controller
+//public class ReviewController {
+//
+//    private final EventRepository eventRepository;
+//
+//    private final ReviewRepository reviewRepository;
+//
+//    private final UserRepository userRepository;
+//
+//
+//    public ReviewController(EventRepository eventRepository, UserRepository userRepository, ReviewRepository reviewRepository) {
+//        this.eventRepository = eventRepository;
+//        this.userRepository = userRepository;
+//        this.reviewRepository = reviewRepository;
+//
+//    }
+//
+//
+//    @GetMapping("/reviews/view/{id}")
+//
+//    public String showEventReview(@PathVariable long id, Model model) {
+//
+//        model.addAttribute("event", eventRepository.findById(id));
+//
+//        model.addAttribute("reviews", reviewRepository.findAllByEventEventId(id));
+//
+//        return "event-reviews";
+//
+//
+//    }
+//
+//
+//}
+//
--- src/main/java/com/example/techconnect/repositories/ReviewRepository.java
+++ src/main/java/com/example/techconnect/repositories/ReviewRepository.java
@@ -12,5 +12,5 @@ import java.util.List;
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
-    List<Review>findAllByEventEventId(long id);
+//    List<Review>findAllByEventEventId(long id);
 }
